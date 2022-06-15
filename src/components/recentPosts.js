0ffde51dbdcc08a1150963da968a47badf3365ed
Beatriz <bitrix24@hotmail.com>
2022-06-15 14:25:10 +0200

    setup redux devtools and fixed reducer for set_recent_posts

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+class RecentPosts extends Component {
+
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
+    
     render() {
         return (
-            <div className="recent-posts">
-                <div className="recent-posts__wrapper">
-                    <div className="recent-posts__heading">Recent Posts</div>
-                    <ul className="recent-posts__posts">
-                        <li>recent post 0</li>
-                        <li>recent post 1</li>
-                        <li>recent post 2</li>
-                    </ul>
-                </div>
-
+        <div className="recent-posts">
+            <div className="recent-posts__wrapper">
+            <div className="recent-posts__heading">Recent Posts</div>
+            <ul className="recent-posts__posts">
+                <li>recent post 0</li>
+                <li>recent post 1</li>
+                <li>recent post 2</li>
+            </ul>
             </div>
+        </div>
         )
     }
 }
 
-export default SearchBar; 
+export default connect(null, actions)(RecentPosts);
