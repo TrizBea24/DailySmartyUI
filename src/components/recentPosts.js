63784fbc09fc8a00690236bfb6625b61fce80f83
Beatriz <bitrix24@hotmail.com>
2022-06-16 19:01:56 +0200

    put together resultsposts component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -32,6 +32,7 @@ class RecentPosts extends Component {
                         {this.renderPosts()}
                     </ul>
                 </div>
+                
             </div>
         )
     }
