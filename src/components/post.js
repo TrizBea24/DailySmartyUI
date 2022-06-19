e4e8c8275e54fc73747766e75dca485525de6962
Beatriz <bitrix24@hotmail.com>
2022-06-19 20:03:25 +0200

    started styling the results and result-post components

--- src/components/post.js
+++ src/components/post.js
@@ -52,10 +52,11 @@ class Post extends Component {
                     </div>
                     <div className="result-post__title">
                         <a href={this.props.url_for_post}
-                            onMouseEnter={() => this.setState({ height: 70 })}
-                            onMouseLeave={() => this.setState({ height: 0 })}
+                                onMouseEnter={() => this.setState({ height: 70 })}
+                                onMouseLeave={() => this.setState({ height: 0 })}
                             >
-                            {this.props.title}</a>
+                            {this.props.title}
+                            </a>
                         </div>
                         <AnimateHeight
                             duration={500}
